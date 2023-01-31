e0f6ec9ef46cd52d4addfbb56e6234434394b900
asmaa <asmaaabdelfattah491@yahoo.com>
2023-01-31 21:15:49 +0200

    tabBar

---
+++ SportsApp_ITI/ViewController/FavouriteViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FavouriteViewController.swift
+//  SportsApp_ITI
+//
+//  Created by Asmaa_Abdelfattah on 11/11/1401 AP.
+//
+
+import UIKit
+
+class FavouriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
