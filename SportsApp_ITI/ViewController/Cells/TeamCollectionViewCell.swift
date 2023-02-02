01df5c99cc163b393dc6a01f91e90e4902705f0e
asmaa <asmaaabdelfattah491@yahoo.com>
2023-02-02 23:14:35 +0200

    Three Collection viwes and youtube

---
+++ SportsApp_ITI/ViewController/Cells/TeamCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TeamCollectionViewCell.swift
+//  SportsApp_ITI
+//
+//  Created by Asmaa_Abdelfattah on 13/11/1401 AP.
+//
+
+import UIKit
+
+class TeamCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var teamImg: UIImageView!
+    
+    @IBOutlet weak var teamName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
